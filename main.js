cf76d03ebb64cadaf22f577ed7c5462ac52d9845
halcombian <103285162+halcombian@users.noreply.github.com>
2022-04-28 14:14:25 -0400

    Add DOM selectors for buttons and play functions

--- main.js
+++ main.js
@@ -3,3 +3,19 @@ let selection = ["rock", "paper", "scissors"];
 function computerSelection() {
   return selection[Math.floor(Math.random() * selection.length)];
 }
+
+const buttonRock = document.getElementById("#btn-rock");
+const buttonPaper = document.getElementById("#btn-paper");
+const buttonScissors = document.getElementById("#btn-scissors");
+
+function playRock() {
+  console.log(selection[0]);
+}
+function playPaper() {
+  return selection[1];
+}
+function playScissors() {
+  return selection[2];
+}
+
+document.getElementById("#btn-rock").addEventListener("click", playRock);
